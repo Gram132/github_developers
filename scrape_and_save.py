7cf5a14db2b085c95454809d11f60dfb8cd205cc
Gram132 <84986668+Gram132@users.noreply.github.com>
2025-02-01 20:21:42 +0100

    commit

--- scrape_and_save.py
+++ scrape_and_save.py
@@ -98,7 +98,7 @@ def main():
     
     
     countries = ["France"]  # Add more countries as needed
-    years = list(range(2022, 2023))
+    years = list(range(2025, 2026))
     followers_ranges = ["<10", "10..50", "50..100", ">100"]
 
     all_developers = []
