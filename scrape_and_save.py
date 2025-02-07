d1641d093c64b90a6226a6bcf8e728a762b40ec5
Gram132 <84986668+Gram132@users.noreply.github.com>
2025-02-07 13:14:45 +0100

    Commit

--- scrape_and_save.py
+++ scrape_and_save.py
@@ -83,7 +83,7 @@ def save_data(data):
         print(f"Error saving data: {str(e)}")
         return {"error": str(e)}
 def main():
-    countries = ["Taiwam","China"]  # Add more countries as needed
+    countries = ["Taiwan","Netherlands"]  # Add more countries as needed
     years = list(range(2008, 2016))
     followers_ranges = ["<10", "10..50", "50..100", ">100"]
 
